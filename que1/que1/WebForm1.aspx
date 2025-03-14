45cb6accbcb12868dac4a0aa62f1d0e6899c6e37
SheladiyaMitanshi <sheladiyamitanshi@gmail.com>
2025-03-14 15:07:23 +0530

    Fixed submodule issue and added que1

---
+++ que1/que1/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="que1.WebForm1" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 85px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Name:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </h1>
+            <h1>Age:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </h1>
+            <h1>Grade:<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            </h1>
+        </div>
+        <asp:Button ID="Button1" runat="server" CssClass="auto-style1" OnClick="Button1_Click" Text="Save" Width="75px" /><br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:TemplateField HeaderText="Delete">
+                    <ItemTemplate>
+                        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" CommandArgument='<%# Eval("Id") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Update">
+                    <ItemTemplate>
+                        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" CommandArgument='<%# Eval("Id") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ que1/que1/WebForm1.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace que1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mitanshi\Documents\School.mdf;Integrated Security=True;Connect Timeout=30");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            print();
+        }
+        public void print()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT [Id], [Name], [Age], [Grade] FROM [Students]", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Button1.Text == "Save")
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO [Students] ([Name], [Age], [Grade]) VALUES (@Name, @Age, @Grade)", con);
+                cmd.Parameters.AddWithValue("Name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("Age",TextBox2.Text);
+                cmd.Parameters.AddWithValue("Grade", TextBox3.Text);
+
+                con.Open();
+                int r=cmd.ExecuteNonQuery();
+                con.Close();
+
+                if(r==1)
+                {
+                    Label1.Text = "Inserted...";
+                    TextBox1.Text = " ";
+                    TextBox2.Text = " ";
+                    TextBox3.Text = " ";
+                    print();
+                }
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE [Students] SET [Name] = @Name, [Age] = @Age, [Grade] = @Grade WHERE [Id] = @Id", con);
+                cmd.Parameters.AddWithValue("Name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("Age", TextBox2.Text);
+                cmd.Parameters.AddWithValue("Grade", TextBox3.Text);
+                cmd.Parameters.AddWithValue("Id", ViewState["Id"]);
+
+                con.Open();
+                int res = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (res == 1)
+                {
+                    Label1.Text = "Updated...";
+                    TextBox1.Text = " ";
+                    TextBox2.Text = " ";
+                    TextBox3.Text = " ";
+                    print();
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            SqlCommand cmd = new SqlCommand("DELETE FROM [Students] WHERE [Id] = @Id", con);
+            cmd.Parameters.AddWithValue("Id", btn.CommandArgument);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Label1.Text = "Deleted";
+            con.Close();
+            print();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            SqlDataAdapter adpt = new SqlDataAdapter("SELECT [Id], [Name], [Age], [Grade] FROM [Students] where Id="+btn.CommandArgument, con);
+            DataTable dt = new DataTable();
+            adpt.Fill(dt);
+            ViewState["Id"] = btn.CommandArgument;
+            TextBox1.Text = dt.Rows[0][1].ToString();
+            TextBox2.Text = dt.Rows[0][2].ToString();
+            TextBox3.Text = dt.Rows[0][3].ToString();
+            Button1.Text = "Updated";
+        }
+    }
+}
